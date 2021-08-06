5a7dac256211f1a72ff8df0651528064671d7b33
Jonathan Zarecki <yonilx@gmail.com>
2021-08-06 18:24:22 +0300

    feat: change importer website to read info from url param
    exporter was also changed to provide an url in the QR code

--- websites/google_calendar_importer_website/run_streamlit.py
+++ websites/google_calendar_importer_website/run_streamlit.py
@@ -1,90 +1,38 @@
 import logging
-import queue
-import threading
-import time
-from pathlib import Path
 
-import cv2
-import numpy as np
 import streamlit as st
-from streamlit_webrtc import ClientSettings, WebRtcMode, webrtc_streamer
-from webcam import webcam
 
-from utils.config import PROJECT_ROOT
-from utils.qr import read_qr_from_ndarray
-
-HERE = Path(__file__).parent
+from utils.google_calendar import create_gc_object, upsert_gc_event_from_outlook_entry
+from utils.outlook_reader.calendar import OutlookCalendarEntry
 
 logger = logging.getLogger(__name__)
 
-WEBRTC_CLIENT_SETTINGS = ClientSettings(
-    rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
-    media_stream_constraints={
-        "video": True,
-        "audio": False,
-    },
-)
+SPLIT_STR = "984651651"
 
 
 def main():
-    st.header("WebRTC demo")
+    st.header("Google Calendar Importer Website")
 
-    app_sendonly_video()
+    query_params = st.experimental_get_query_params()
 
-    logger.debug("=== Alive threads ===")
-    for thread in threading.enumerate():
-        if thread.is_alive():
-            logger.debug(f"  {thread.name} ({thread.ident})")
+    if "qr_str" in query_params:
+        exporter_str = query_params["qr_str"][0]
+        assert (
+            exporter_str[: len(st.secrets["unique_identifier"])] == st.secrets["unique_identifier"]
+        ), "QR code was generated with this id at the beginning"
 
+        exporter_str = exporter_str[len(st.secrets["unique_identifier"]) :]
+        entry_list = [OutlookCalendarEntry.import_from_char(repr_s) for repr_s in exporter_str.split(SPLIT_STR)]
+        st.text(str(entry_list))
 
-def app_sendonly_video():
-    """A sample to use WebRTC in sendonly mode."""
-    webrtc_ctx = webrtc_streamer(
-        key="media-constraints",
-        mode=WebRtcMode.SENDONLY,
-        audio_receiver_size=0,
-        video_receiver_size=50,
-        client_settings=WEBRTC_CLIENT_SETTINGS,
-    )
+        gc = create_gc_object()
 
-    image_place = st.empty()
-    text_place = st.empty()
-    img = webcam()
-    if img is None:
-        st.write("Waiting for capture...")
+        for outlook_entry in entry_list:
+            print(outlook_entry)
+            upsert_gc_event_from_outlook_entry(gc, outlook_entry)
+
     else:
-        img_rgb = img.__array__()
-        st.write("Got an image from the webcam:")
-        fname = os.path.join(PROJECT_ROOT, "examples", "qr_images", "from_webcam.png")
-        cv2.imwrite(fname, img_rgb)
-        image_place.image(img_rgb)
-        decoded_qr = read_qr_from_ndarray(img_rgb)
-
-        text_place.text(decoded_qr if decoded_qr is not None else f"QR not found - {time.time()}")
-        logger.warning(decoded_qr)
-
-    while True:
-        if webrtc_ctx.video_receiver:
-            try:
-                video_frame = webrtc_ctx.video_receiver.get_frame(timeout=1)
-            except queue.Empty:
-                logger.warning("Queue is empty. Abort.")
-                break
-
-            img_rgb: np.ndarray = video_frame.to_ndarray(format="rgb24")
-            image_place.image(img_rgb)
-
-            decoded_qr = read_qr_from_ndarray(img_rgb)
-            if decoded_qr is not None:
-                fname = os.path.join(PROJECT_ROOT, "examples", "qr_images", "from_webcam.png")
-
-                cv2.imwrite(fname, img_rgb)
-            text_place.text(decoded_qr if decoded_qr is not None else f"QR not found - {time.time()}")
-            logger.warning(decoded_qr)
-
-        else:
-            logger.warning("AudioReceiver is not set. Abort.")
-            break
+        st.text("no qr_str")
 
 
 if __name__ == "__main__":
@@ -98,11 +46,5 @@ if __name__ == "__main__":
     )
 
     logger.setLevel(level=logging.DEBUG if DEBUG else logging.INFO)
-
-    st_webrtc_logger = logging.getLogger("streamlit_webrtc")
-    st_webrtc_logger.setLevel(logging.DEBUG)
-
-    fsevents_logger = logging.getLogger("fsevents")
-    fsevents_logger.setLevel(logging.WARNING)
 
     main()
--- websites/outlook_exporter_website/run_streamlit.py
+++ websites/outlook_exporter_website/run_streamlit.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import urllib3.request
 
 from utils.outlook_reader.calendar import (
     get_current_user_outlook_calendar,
@@ -7,7 +8,6 @@ from utils.outlook_reader.calendar import (
 from utils.qr import create_qr_image
 
 if __name__ == "__main__":
-
     st.header("Outlook Exporter")
     calendar = get_current_user_outlook_calendar()
     entries = read_calendar(calendar)
@@ -16,4 +16,7 @@ if __name__ == "__main__":
     exported_str = st.secrets["unique_identifier"] + SPLIT_STR.join([ent.export_as_str() for ent in entries])
     st.text(exported_str)
 
-    st.image(create_qr_image(exported_str), caption="qr code")
+    st.image(
+        create_qr_image("http://172.16.0.116:8501?" + urllib3.request.urlencode({"qr_str": exported_str})),
+        caption="qr code",
+    )
